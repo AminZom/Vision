ea45f11c57d05b9fa37c91ef8a91b6c1697645f8
Soufia Naseri <52929241+Sofia-git@users.noreply.github.com>
2021-09-26 18:50:08 -0400

    augmentation changes

--- Apps/database/utils/image_augmentation.py
+++ Apps/database/utils/image_augmentation.py
@@ -1,2 +1,8 @@
 import Augmentor
-p = Augmentor.Pipeline("./circles_samples")
+p = Augmentor.Pipeline("./Apps/database/circles_samples")
+#  no cropping, be careful of zoom feature
+p.rotate_without_crop(probability=1, max_left_rotation=180, max_right_rotation=180)
+p.flip_left_right(probability=0.5)
+p.flip_top_bottom(probability=0.5)
+p.zoom_random(probability=0.3, percentage_area=0.8)  # test once running on real images
+p.sample(50)
