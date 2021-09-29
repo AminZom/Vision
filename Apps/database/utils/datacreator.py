f2bedfcc41fd2df50b252d1661d031ef83881efc
Aria Sheikhtaheri <aria.staheri@gmail.com>
2021-09-29 00:33:42 -0400

    data creation partially completed

--- Apps/database/utils/datacreator.py
+++ Apps/database/utils/datacreator.py
@@ -3,69 +3,150 @@ Dataset creator pipeline
 
 """
 # imports
+import numpy as np
+import sys
+from PIL import Image, ImageDraw
+from random import randint
+from math import sqrt
+
 
 class circle_gen():
+ 
+    def __init__(self, width, height, count_light, count_dark, path_to_images):
+        self.image_count_light = count_light
+        self.image_count_dark = count_dark
+        self.path_to_images = path_to_images
+        self.current_file_count = 1
+        self.width = width
+        self.height = height
+
 
     def min_max(self):
         # number of creating objects in mind
-        return None
+        return randint(5, 15)
 
-    def change_contrast(self):
+
+    def create_image_with_background(self, background_color):
         # background, foreground change all in grayscale
-        #
-        return None
+        red = 0
+        blue = 0
+        green = 0
 
-    def save_img(self):
-        return None
+        if(background_color == "light"):
+            red = randint(0, 30)
+            blue = randint(0, 30)
+            green = randint(0, 30)
+        elif (background_color == "dark"):
+            red = randint(160, 240)
+            blue = randint(160, 240)
+            green = randint(160, 240)
+            
+        image = Image.new('RGBA', (self.width, self.height), (red, blue, green))
+        return image
 
-    def save_annotation(self):
+
+    def save_img(self, image):
+        image.save(self.path_to_images + "/circles_training_dataset_" + str(self.current_file_count) + ".png")
+        self.current_file_count = self.current_file_count + 1     
+
+
+    def save_annotation(self, data):
         # save center (x, y), radius
-        pass
+        [x, y, r] = data
 
-    def circle_width(self):
-        pass
+        with open("../dataset/test/annotations/annotations.txt", "a") as annotations:
+            line = "[(" + str(x) + ", " + str(y) + "), " + str(r) + "]\n"
+            annotations.write(line) 
 
-    def random_circles(self):
+        annotations.close()
+
+
+    def circle_radius_set(self):
+        return randint(5, 50)
+
+    
+    def random_center_position_and_radius(self):
+        x = randint(25, 575)
+        y = randint(25, 375)
+        r = self.circle_radius_set()
+        return [x, y, r]
+
+    def draw_random_circles(self):
         # circle size change, numbers, contrast, then save_img, then save_annotation
-        pass
+        circles_created = []
+        for i in range(self.image_count_light):
+            print("inside light, i = " + str(i))
+            new_image = self.create_image_with_background("light")
+            draw = ImageDraw.Draw(new_image)
+            for y in range(self.min_max()):
+                print("inside for in light, y = " + str(y))
+                while True:               
+                    [x, y, r] = self.random_center_position_and_radius()
+                    if(len(circles_created) == 0 or not (self.is_intersecting_some_circle_or_boundary(circles_created, [x, y, r]))):
+                        draw.ellipse((x - r, y - r, x + r, y + r), fill=self.generate_fill("dark"), width=1)
+                        circles_created.append([x, y, r])
+                        self.save_annotation([x, y, r])
+                        break
+            self.save_img(new_image)
+        for i in range(self.image_count_dark):
+            print("inside dark, i = " + str(i))
+            new_image = self.create_image_with_background("dark")
+            draw = ImageDraw.Draw(new_image)
+            for y in range(self.min_max()):
+                print("inside for in dark, y = " + str(y))
+                while True:               
+                    [x, y, r] = self.random_center_position_and_radius()
+                    if(len(circles_created) == 0 or not (self.is_intersecting_some_circle_or_boundary(circles_created, [x, y, r]))):
+                        draw.ellipse((x - r, y - r, x + r, y + r), fill=self.generate_fill("light"), width=1)
+                        circles_created.append([x, y, r])
+                        self.save_annotation([x, y, r])
+                        break
+            self.save_img(new_image)
 
-import numpy as np
-from shapely.geometry.point import Point
-from skimage.draw import circle_perimeter_aa
-import sys
+
+    def is_intersecting_some_circle_or_boundary(self, array_of_circles, new_circle):
+        for circle in array_of_circles:
+            if(self.are_two_circles_intersecting(new_circle, circle) or
+            self.is_intersecting_boundary(new_circle)):
+                return True           
+        return False
 
 
-def draw_circle(img, row, col, rad):
-    rr, cc, val = circle_perimeter_aa(row, col, rad)
-    valid = (
-        (rr >= 0) &
-        (rr < img.shape[0]) &
-        (cc >= 0) &
-        (cc < img.shape[1])
-    )
-    img[rr[valid], cc[valid]] = val[valid]
+    def are_two_circles_intersecting(self, new_circle, circle):
+        [new_x, new_y, new_r] = new_circle
+        [x, y, r] = circle
 
-def iou(params0, params1):
-    row0, col0, rad0 = params0
-    row1, col1, rad1 = params1
+        dist_between_centers = sqrt(((new_x - x) ** 2) + ((new_y - y) ** 2))
 
-    shape0 = Point(row0, col0).buffer(rad0)
-    shape1 = Point(row1, col1).buffer(rad1)
+        return dist_between_centers <= (new_r + r)
 
-    return (
-        shape0.intersection(shape1).area /
-        shape0.union(shape1).area
-    )
 
-def noisy_circle(size, radius, noise):
-    img = np.zeros((size, size), dtype=np.float)
+    def is_intersecting_boundary(self, new_circle):
+        [new_x, new_y, new_r] = new_circle
 
-    # Circle
-    row = np.random.randint(size)
-    col = np.random.randint(size)
-    rad = np.random.randint(10, max(10, radius))
-    draw_circle(img, row, col, rad)
+        return ((new_x + new_r >= self.width) or (new_x - new_r <= 0) or 
+                (new_y + new_r >= self.height) or (new_y - new_r <= 0))
 
-    # Noise
-    img += noise * np.random.rand(*img.shape)
-    return (row, col, rad), img
+
+    def generate_fill(self, contrast):
+        red = 0
+        blue = 0
+        green = 0
+
+        if(contrast == "light"):
+            red = randint(0, 30)
+            blue = randint(0, 30)
+            green = randint(0, 30)
+        elif (contrast == "dark"):
+            red = randint(160, 240)
+            blue = randint(160, 240)
+            green = randint(160, 240)
+        
+        return (red, blue, green)
+
+
+#main start of application for image generation
+
+circle_files = circle_gen(600, 400, 1, 1, "../dataset/test/new_circles")
+circle_files.draw_random_circles()
+
